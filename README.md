9768b3e400b768edf4d7028e9c241d92f41a756d
unknown <sv73107535@idat.edu.pe>
2021-09-06 19:47:23 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+FUNDAMENTOS DE JAVASCRIPT
+Comprendiendo Iteración y Bucles en Javascript
+sotopro/Sm3_Sesion_01_RNative04: FUNDAMENTOS DE PROGRAMACIÓN EN JAVASCRIPT - Comprendiendo Iteración y Bucles en JavaScript (github.com)
---
+++ index.js
@@ -0,0 +1,79 @@
+let poplimit = 20;
+
+let fish = 0;
+
+while (fish < poplimit) {
+    fish++;
+    console.log(`There's room for ${poplimit - fish} more fish` )
+};
+
+// initiate an infinite loop
+
+//while (true) {
+    // execute code forever
+//}
+
+//set a conditio to true
+
+const iceCapsAreMelting= true
+let polarBears = 8;
+while(iceCapsAreMelting) {
+    console.log(`There are ${polarBears} polar bears.`);
+    polarBears--;
+    if (polarBears === 0) {
+        console.log("There are no polar bears left.")
+        break;
+    }
+}
+//set variable to 0
+let x = 0
+
+do {
+    x ++;
+    console.log(x);
+} while(false)
+
+let i = 0;
+do {
+    console.log('Hello world!');
+    i ++;
+} while (i < 5)
+
+// for loop
+
+// for(initializatiom; condition; final expression/ incremetation) {
+    //execute code
+// }
+
+for (let i = 0; i < 10; i++){
+    console.log(i)
+}
+// for (; ; i++) {
+    // if (i > 5){
+        //break
+    //}
+    // console.log(i);
+//}
+
+//initialize empty array
+const arrayExample = [];
+
+for (let i = 0; i < 6; i++) {
+    arrayExample.push(i);
+    console.log(arrayExample)
+}
+
+let fishList = ['goldenfish', 'clownfish', 'catfish', 'bluegill', 'pufferfish'];
+
+for (let i = 0; i <fishList.length; i++) {
+    console.log(fishList[i])
+}
+
+const shark = {
+    species: 'hammerhead',
+    color: 'gray',
+    numberOfTeeth: Infinity
+}
+for (atribute in shark){
+    console.log(atribute)
+}
